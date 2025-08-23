24e3c5027fa490a819af2bf89cef36e72f049e4f
manky136 <mayankkumar5133@gmail.com>
2025-08-23 19:02:18 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,16 @@
+const accountId = 23283
+let accountEmail = "abcd@gmail.com"
+var accountPassword = "5465"
+accountCity = "jaipur"
+let accountState; 
+
+// console.log(accountId)
+
+accountEmail = "dbsa@gmail.com"
+accountPassword = "4652"
+accountCity = "new york"
+
+/*dont use var because of 
+ block scope and functional scope*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+let name = "hitesh"
+let age = 19
+let isLoggedIn = true
+
+
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//undefined
+//symbols => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
