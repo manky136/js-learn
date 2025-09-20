e2c8e918de6cd77a090439de44f9fcb344ce1356
manky136 <mayankkumar5133@gmail.com>
2025-09-20 16:53:01 +0000

    obj1

---
+++ 02basics/04objects.js
@@ -0,0 +1,24 @@
+// cont tineUe = ne Object
+const tinderUser = {}
+
+tinderUser.id =  "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "khan"
+
+        }
+        
+    }
+}
+
+//console.log(regularUser.fullname.userfullname);
+
+const obj1
