fde18c0da09ccfae71613dbf32c47b8228e0b4f0
manky136 <mayankkumar5133@gmail.com>
2025-09-20 20:51:27 +0000

    api

--- 02basics/04objects.js
+++ 02basics/04objects.js
@@ -21,4 +21,62 @@ const regularUser = {
 
 //console.log(regularUser.fullname.userfullname);
 
-const obj1
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 5: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1[1], ...obj2}
+console.log(obj3)
+
+
+const users = [
+    {
+        id: "dcd",
+        email: "sdf@vx.com"
+    },
+    {
+        id: "dcd",
+        email: "sdf@vx.com"
+    },
+    {
+        id: "dcd",
+        email: "sdf@vx.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor: "manky"
+}
+
+//course.courseInstructot
+
+const{courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name" : "hitesh",
+//     "coursename" : "js in hindi",
+//     "price": "free";
+// }
+
+[
+    {},
+    {},
+    {}
+]
